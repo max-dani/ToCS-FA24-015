542bff873a75aa111a220644e1e71545c311e89e
danish tufail <152505304+max-dani@users.noreply.github.com>
2024-12-09 14:18:31 +0500

    Create SquareCalculator.java

---
+++ SquareCalculator.java
@@ -0,0 +1,9 @@
+public class SquareCalculator {
+    public static int square(int number) {
+        return number * number;
+    }
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("Square of " + number + " is: " + square(number));
+    }
+}
